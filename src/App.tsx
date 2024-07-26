66dabec87c1c615f1243f7dd1681db89b09cf481
pow4h <44829224+kubas33@users.noreply.github.com>
2024-07-26 11:50:20 +0000

    Dodanie sass oraz stylów

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,57 @@
-import React, {Suspense, useState} from 'react'
-import './App.css'
-import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
-import {CSpinner} from "@coreui/react";
+import React, { Suspense, useState } from "react";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { CSpinner } from "@coreui/react";
 import MainNavigation from "./layout/MainNavigation.tsx";
-import {ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import DefaultLayout from "./layout/DefaultLayout.tsx";
+import "./scss/style.scss";
 
-const Login = React.lazy(() => import('./views/auth/login/Login.tsx'))
-// const Register = React.lazy(() => import('./views/auth/register/Register.jsx'))
-const Page404 = React.lazy(() => import('./views/errors/page404/Page404.tsx'))
-const Page500 = React.lazy(() => import('./views/errors/page500/Page500.tsx'))
+const Login = React.lazy(() => import("./views/auth/login/Login.tsx"));
+const Register = React.lazy(() => import("./views/auth/register/Register.tsx"));
+const Page404 = React.lazy(() => import("./views/errors/page404/Page404.tsx"));
+const Page500 = React.lazy(() => import("./views/errors/page500/Page500.tsx"));
 
 function App() {
-
-
   return (
-      <>
-          <MainNavigation>
-              <BrowserRouter>
-                  <Suspense
-                      fallback={
-                          <div className="pt-3 text-center">
-                              <CSpinner color="primary" variant="grow"/>
-                          </div>
-                      }
-                  >
-                      <Routes>
-                          <Route path="/login" name="Login Page" element={<Login/>}/>
-                          {/*<Route path="/register" name="Register Page" element={<Register/>}/>*/}
-                          <Route path="/404" name="Page 404" element={<Page404/>}/>
-                          <Route path="/500" name="Page 500" element={<Page500/>}/>
-                          <Route path="/" element={<DefaultLayout/>}/>
-                          <Route path="*" element={<Page404/>}/>
-                      </Routes>
-                  </Suspense>
-              </BrowserRouter>
-          </MainNavigation>
+    <>
+      <MainNavigation>
+        <BrowserRouter>
+          <Suspense
+            fallback={
+              <div className="pt-3 text-center">
+                <CSpinner color="primary" variant="grow" />
+              </div>
+            }
+          >
+            <Routes>
+              <Route path="/login" name="Login Page" element={<Login />} />
+              <Route
+                path="/register"
+                name="Register Page"
+                element={<Register />}
+              />
+              <Route path="/404" name="Page 404" element={<Page404 />} />
+              <Route path="/500" name="Page 500" element={<Page500 />} />
+              <Route path="/" element={<DefaultLayout />} />
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </MainNavigation>
 
-          <ToastContainer
-              position="top-right"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-          />
-      </>
-
-  )
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
